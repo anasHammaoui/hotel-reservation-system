1a14bccd3b1e35c130768af6c1cef649e9c8242f
anashm <anashammaoui07@gmail.com>
2025-09-19 14:49:32 +0100

    add reservation model

---
+++ src/models/Reservation.java
@@ -0,0 +1,57 @@
+package src.models;
+import java.sql.Timestamp;
+import java.util.UUID;
+
+public class Reservation {
+    private UUID id;
+    private Timestamp instant;
+    private UUID hotelId;
+    private UUID clientId;
+    private int nights;
+    
+    public Reservation(Timestamp instant, UUID hotelId, int nights ){
+        this.id = UUID.randomUUID();
+        this.instant = instant;
+        this.hotelId = hotelId;
+        this.nights = nights;
+    }
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public Timestamp getInstant() {
+        return instant;
+    }
+
+    public void setInstant(Timestamp instant) {
+        this.instant = instant;
+    }
+
+    public UUID getHotelId() {
+        return hotelId;
+    }
+
+    public void setHotelId(UUID hotelId) {
+        this.hotelId = hotelId;
+    }
+
+    public UUID getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(UUID clientId) {
+        this.clientId = clientId;
+    }
+
+    public int getNights() {
+        return nights;
+    }
+
+    public void setNights(int nights) {
+        this.nights = nights;
+    }
+}
